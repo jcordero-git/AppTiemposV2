b4fd4ef6cfa9b5f1560291c0076764db5934be29
Jose Luis Cordero <jocorbre@gmail.com>
2025-06-09 02:38:56 -0600

    arreglo de parseo del msj de extraccion de texto AI

--- utils/UtilParseMessageAI.js
+++ utils/UtilParseMessageAI.js
@@ -2,6 +2,7 @@ const API_KEY = "AIzaSyDjpMYWwqd3Zr9s2xS2oWpRjdG69yfzsbs"; // <-- reemplaza con 
 const ENDPOINT = `https://generativelanguage.googleapis.com/v1/models/gemini-2.0-flash:generateContent?key=${API_KEY}`;
 
 export async function parseMessage(text, day, extraPrompt) {
+  let cleaned;
   try {
     const prompt = `Hoy es: ${day}, ${extraPrompt}${text.trim().replace(/"/g, '\\"')}"`;
 
@@ -31,14 +32,17 @@ export async function parseMessage(text, day, extraPrompt) {
     const textResponse =
       responseJson.candidates?.[0]?.content?.parts?.[0]?.text || "";
 
-    const cleaned = textResponse
+    cleaned = textResponse
       .replace(/```json\s*/g, "")
       .replace(/```/g, "")
-      .trim();
-
+      .trim()
+      .replace(/"numero": (\d{1})\b/g, '"numero": "0$1"') // 1 dígito → agrega cero
+      .replace(/"numero": (\d{2})\b/g, (_, d) => `"numero": "${d}"`); // 2 dígitos como string
+    console.log("Mensaje cleaned:", cleaned);
     return JSON.parse(cleaned); // Devuelve como arreglo de objetos
   } catch (error) {
     console.error("Error al parsear mensaje:", error);
+    console.error("Error al parsear mensaje cleaned:", cleaned);
     return [];
   }
 }
